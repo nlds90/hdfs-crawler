9c392e2c9300cf3eb124f062f20b171cba0eea32
Tung Phan <nlds.a279@gmail.com>
2019-03-28 17:57:59 +0700

    Add hdfs parquet indexing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pyarrow import hdfs
 from pyarrow import csv
-from pyarrow import parquet
 import requests
 import os
 
@@ -14,7 +13,9 @@ def scan():
         if fs.isdir(dataset):
             print("CREATE DATASET", dataset)
             dataset_id = create_dataset(dataset)
-            index_dataset(dataset_id, dataset)
+
+            if (dataset_id):
+                index_dataset(dataset_id, dataset)
 
 def create_dataset(dataset_path):
     query = '''
@@ -33,8 +34,7 @@ def create_dataset(dataset_path):
 
     r = graphql(query, variables)
     dataset = r.json()
-    print("GRAPHQL DATASET", dataset)
-    return dataset.get("data").get("datasetUpsertFromStorage").get("id");
+    return dataset.get("data", {}).get("datasetUpsertFromStorage", {}).get("id");
 
 def graphql(query, variables):
     payload = {
@@ -47,31 +47,40 @@ def graphql(query, variables):
 def index_dataset(dataset_id, dataset):
     for file in fs.ls(dataset):
         if fs.isfile(file):
-            file_name, file_ext = os.path.splitext(file)
-            if file_ext == ".csv":
-                print("INDEX CSV", file)
-                index_csv(dataset_id, file)
-            elif file_ext == ".parquet":
-                print("INDEX PARQUET", file)
-                index_parquet(dataset_id, file)
-            else:
-                print("Unsupported File!")
+            index_file(dataset_id, file)
         else:
-            print("TRY TO READ PARQUET DIR")
-            try:
-                index_parquet_dir(dataset_id, file)
-            except:
-                index_dataset(dataset_id, file)
+            index_dir(dataset_id, file)
 
+def index_file(dataset_id, file):
+    try:
+        file_name, file_ext = os.path.splitext(file)
+        if file_ext == ".csv":
+            print("INDEX CSV", file)
+            index_csv(dataset_id, file)
+        elif file_ext == ".parquet":
+            print("INDEX PARQUET", file)
+            index_parquet(dataset_id, file)
+        else:
+            print("UNSUPPORTED FILE", file)
+            print('============')
+    except Exception as e:
+        print('ERROR:', e)
+        print('============')
+
+def index_dir(dataset_id, directory):
+    try:
+        print("INDEX DIR", directory)
+        index_parquet(dataset_id, directory)
+    except Exception as e:
+        print('ERROR:', e)
+        print('============')
+        index_dataset(dataset_id, directory)
 
 def index_csv(dataset_id, file):
-    try:
-        with fs.open(file) as csv_file:
-            table = csv.read_csv(csv_file)
-            csvHeader = [{ "name": column.name } for column in table.columns]
-            create_file({ "datasetId": dataset_id, "pathFromRoot": file, "csvHeader": csvHeader })
-    except Exception as e:
-        print(e)
+    with fs.open(file) as csv_file:
+        table = csv.read_csv(csv_file)
+        csv_header = [{ "name": column.name, "description": str(column.type) } for column in table.columns]
+        create_file({ "datasetId": dataset_id, "pathFromRoot": file, "csvHeader": csv_header })
 
 def create_file(record):
     query = '''
@@ -85,83 +94,15 @@ def create_file(record):
     variables = {
         "token": HASBRAIN_TOKEN,
         "upsert": { "datasetId": record.get("datasetId"), "pathFromRoot": record.get("pathFromRoot") },
-        "record": { "csvHeader": record.get("csvHeader") }
+        "record": { "csvHeader": record.get("csvHeader"), "metadata": record.get("metadata") }
     }
 
-    r = graphql(query, variables)
-    print("GRAPHQL FILE", r.json())
-
-def getTypeFromPhysicalType(dtype):
-    type_map = {
-        'INT64': 'INTEGER',
-        'INT32': 'INTEGER',
-        'INT96': 'INTEGER',
-        'INT8': 'INTEGER',
-        'FLOAT': 'DECIMAL',
-        'DOUBLE': 'DECIMAL',
-        'FLOAT': 'DECIMAL',
-        'BOOLEAN': 'BOOLEAN',
-        'DATE': 'DATE',
-    }
-    known_type = type_map.get(str(dtype))
-    return known_type if known_type else 'STRING'
-
-def getTypeFromLogicalType(dtype):
-    type_map = {
-       # 'UTF8': 'STRING',
-        'UINT_8': 'INTEGER',
-        'UINT_16': 'INTEGER',
-        'UINT_32': 'INTEGER',
-        'UINT_64': 'INTEGER',
-        'INT_8': 'INTEGER',
-        'INT_16': 'INTEGER',
-        'INT_32': 'INTEGER',
-        'INT_64': 'INTEGER',
-        'DECIMAL': 'DECIMAL',
-        'DATE': 'DATE',
-    }
-    known_type = type_map.get(str(dtype))
-    return known_type if known_type else 'STRING'
-
-def getType(physical_type, logical_type):
-    if (logical_type == 'NONE'):
-        return getTypeFromPhysicalType(physical_type)
-    else:
-        return getTypeFromLogicalType(logical_type)
-
-def index_parquet_dir(dataset_id, dir):
-    dataset = parquet.ParquetDataset(dir)
-    metadata = {
-        'num_rows': dataset.metadata.num_rows,
-        'num_row_groups': dataset.metadata.num_row_groups,
-        'serialized_size': dataset.metadata.serialized_size
-    }
-    print(metadata)
-    parquet_header = []
-    for item in dataset.schema:
-        parquet_header.append({
-            'name': item.name,
-            'type': getType(item.physical_type, item.logical_type)
-        })
-        # physical_type logical_type
-    print(parquet_header)
+    return graphql(query, variables)
 
 def index_parquet(dataset_id, file):
-    parquet_file = parquet.ParquetFile(file)
-    metadata = {
-        'num_rows': parquet_file.metadata.num_rows,
-        'num_row_groups': parquet_file.metadata.num_row_groups,
-        'serialized_size': parquet_file.metadata.serialized_size
-    }
-    print(metadata)
-    parquet_header = []
-    for item in parquet_file.schema:
-        parquet_header.append({
-            'name': item.name,
-            'type': getType(item.physical_type, item.logical_type)
-        })
-        # physical_type logical_type
-    print(parquet_header)
+    table = fs.read_parquet(file)
+    parquet_header = [{ "name": column.name, "description": str(column.type) } for column in table.columns]
+    create_file({ "datasetId": dataset_id, "pathFromRoot": file, "metadata": parquet_header })
 
 scan()
 
